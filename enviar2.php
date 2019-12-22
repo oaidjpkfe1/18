178ffb671a4c915152db3e32e771c7cce7cce992
your name its frikky <eduardo192@gmail.com>
2019-12-22 09:17:37 -0800

    jubhska

---
+++ enviar2.php
@@ -0,0 +1,20 @@
+<?php
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $correo . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $correo . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$envio = mail('lusigonzales06@gmail.com', $nombre, $correo, $correo, $header);
+
+
+header("Location:gracias.html");
+?>
